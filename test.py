016e74cd475a58bc03eb1c3bc3ae9752757fa44a
chen-xin-94 <xinchenqd@gmail.com>
2021-04-03 23:33:38 +0200

    Pycharm 4

---
+++ test.py
@@ -0,0 +1,24 @@
+from random import choice
+class Person:
+
+    def __init__(self, name):
+        self.greeting = 'Hello {name}'
+        self.name = name
+
+    def __str__(self):
+        return self.make_greeting()
+
+    def make_greeting(self):
+        return self.greeting.format(name=self.name)
+
+
+def main():
+    people = [
+        Person('A'),
+        Person('B')
+    ]
+    person = choice(people)
+    print(person)
+
+if __name__ == '__main__':
+    main()
